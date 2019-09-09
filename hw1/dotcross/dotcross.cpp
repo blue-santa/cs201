5d113f71e36842bd6e6d1c15eeed58572a2a04e1
Siddhartha Crypto <siddhartha-crypto@protonmail.com>
2019-09-08 20:08:44 -0800

    add documentation to dotcross.cpp

--- hw1/dotcross/dotcross.cpp
+++ hw1/dotcross/dotcross.cpp
@@ -14,9 +14,15 @@ using namespace std;
 
 int main() {
 
+    // Declare a vector to hold user inputs
+
     vector<float> dotcross;
-    float input_A;
-    float input_B;
+
+    // Declare a float variable to hold user inputs
+
+    float input;
+
+    // Declare a vector to hold the variables names
 
     vector<string> xyz;
     xyz.push_back("Ax");
@@ -26,26 +32,42 @@ int main() {
     xyz.push_back("By");
     xyz.push_back("Bz");
 
+    // Request user input
+
     cout << "Provide three floating point numbers for vector A and three more for vector B: " << endl;
 
+    // Initiate a while loop that continues until the user has provided all six inputs for vectors A and B
+
     while (dotcross.size() < 6) {
+
+        // Request user input for the current vector variable
+
         cout << xyz.at(dotcross.size()) << ": ";
-        cin >> input_A;
+        cin >> input;
+
+        // Test whether user input is valid, and if not, repeat the while loop
+        // Otherwise, add the user input to the vector variables
 
         if (cin.fail()) {
             cin.clear();
             cin.ignore(1000,'\n');
             cout << "The input you provided is not valid. Please try again." << endl;
         } else {
-            dotcross.push_back(input_A);
+            dotcross.push_back(input);
         }
     }
 
+    // Declare a dot product variable and perform the dot-product calculation
+
     float dotprod = dotcross.at(0) * dotcross.at(3) + dotcross.at(1) * dotcross.at(4) + dotcross.at(2) * dotcross.at(5);
+
+    // Delcare a vector of the cross-product vector and perform each calculation
     vector<float> crossprod;
     crossprod.push_back(dotcross.at(1) * dotcross.at(5) - dotcross.at(2) * dotcross.at(4));
     crossprod.push_back(dotcross.at(2) * dotcross.at(3) - dotcross.at(0) * dotcross.at(5));
     crossprod.push_back(dotcross.at(0) * dotcross.at(4) - dotcross.at(1) * dotcross.at(3));
+
+    // Report the results
 
     cout << fixed << setprecision(5) <<  "A dot B = " << dotprod << endl;
     cout << "A cross B = (" << crossprod.at(0) << ", " << crossprod.at(1) << ", " << crossprod.at(2) << ")" << endl;
