9cf26d8268dc680b2a691b46822e6c4121d4df92
Siddhartha Crypto <siddhartha-crypto@protonmail.com>
2019-09-08 20:30:58 -0800

    add documentation to mileskm

--- hw1/mileskm/mileskm.cpp
+++ hw1/mileskm/mileskm.cpp
@@ -12,18 +12,28 @@ using namespace std;
 
 int main() {
 
+    // Set a default conversion value for kilometers
+
     float km_per_m = 1.609;
 
+    // Declare a variable for user input
+
     float input;
+
+    // Request user input
 
     cout << "Enter the number of miles to convert to kilometers: ";
 
     cin >> input;
 
+    // Test that the user input is valid, and if not, end the program
+
     if (cin.fail()) {
         cout << "The value you entered is not a valid number. Please enter a numerical value." << endl;
         return 0;
     } 
+
+    // Return the final result and calculations
 
     cout << "The number of kilometers in " << input << " miles is: " << input * km_per_m << endl;
 
