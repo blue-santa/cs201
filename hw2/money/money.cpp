7e704b5496500ef56baca8eea9ad236297cd4e1d
siddhartha-crypto <siddhartha-crypto@protonmail.com>
2019-09-15 12:33:23 -0800

    Add comments

--- hw2/money/money.cpp
+++ hw2/money/money.cpp
@@ -27,34 +27,45 @@ void waitForContinue() {
     getchar();
 }
 
+// Inform the user their input is invalid
+
 void askUserAgain() {
 
     std::cout << "You provided an invalid input. Please try again." << std::endl << std::endl;;
 
 }
 
+// Query the user to input their wallet state
+
 void queryUserWallet(std::vector<int> & user_wallet, std::vector<std::string> & coin_list_plural) {
+
+    // Declare an input variable for user input
 
     int input;
 
+    // Request the user to input the total number of each coin they have in their wallet
 
     for (int i = 0; i < coin_list_plural.size(); i++) {
 
         clearConsole();
 
-        std::cout << "In the array there are: " << coin_list_plural.size() << std::endl;
-
         std::cout << "How many " << coin_list_plural.at(i) << " do you have? ";
+
+        // Initiate a while loop to wait until the user inputs a viable response
 
         while (true) {
 
             std::cin >> input;
+
+            // If the response is invalid, ask again
 
             if (std::cin.fail() || input < 0) {
                 std::cin.clear();
                 std::cin.ignore(1000, '\n');
                 askUserAgain();
                 waitForContinue();
+
+                // If the response is valid, input the value and move to the next iteration of the for loop
 
             } else {
                user_wallet.push_back(input);
@@ -66,9 +77,13 @@ void queryUserWallet(std::vector<int> & user_wallet, std::vector<std::string> & 
     } 
 }
 
+// Calculate the wallet total as a floating point variable
+
 float calculateWalletTotal(std::vector<int> & user_wallet) {
 
     float total_wallet = 0;
+
+    // Initiate the various values of the coins
 
     std::vector<int> values;
         values.push_back(1);
@@ -76,19 +91,32 @@ float calculateWalletTotal(std::vector<int> & user_wallet) {
         values.push_back(10);
         values.push_back(25);
 
+        // Calcuate the total value of the wallet in pennies
+
     for (int i = 0; i < 4; i++) {
         total_wallet = total_wallet + user_wallet.at(i) * values.at(i);
     }
 
+    // Transform the total value into a dollar value
+
     total_wallet = total_wallet * 0.01;
+    
+    // Return the total value
+
     return total_wallet;
 }
+
+// Print to the console the total wallet sum
 
 void reportWalletSum(std::vector<int> & user_wallet, std::vector<std::string> & coin_list_plural, std::vector<std::string> & coin_list_singular) {
 
     clearConsole();
 
+    // Call the calculateWalletTotal() function to calculate the wallet total
+
     float total_wallet = calculateWalletTotal(user_wallet);
+
+    // For each coin type, print the total in the user's wallet
 
     for (int i = 0; i < 4; i++) {
 
@@ -104,7 +132,11 @@ void reportWalletSum(std::vector<int> & user_wallet, std::vector<std::string> & 
 
     }
 
+    // Print the total value in the wallet
+
     std::cout << "The value of all your coins is $" << total_wallet << std::endl; 
+
+    // Pause for user to continue
 
     waitForContinue();
 
@@ -112,7 +144,11 @@ void reportWalletSum(std::vector<int> & user_wallet, std::vector<std::string> & 
 
 int main(int argc, char **argv) {
 
+    // Clear the console
+
     clearConsole();
+
+    // Declare the vector to hold the user's coin totals
 
     std::vector<int> user_wallet;
 
@@ -132,9 +168,15 @@ int main(int argc, char **argv) {
         coin_list_singular.push_back("dime");
         coin_list_singular.push_back("quarter");
 
+        // Query the user's wallet
+
     queryUserWallet(user_wallet, coin_list_plural);
 
+    // Clear the console
+
     clearConsole();
+
+    // Report the total value
 
     reportWalletSum(user_wallet, coin_list_plural, coin_list_singular);
 
