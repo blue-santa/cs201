c57cfffda8d1b97bd57db4c343cf519f6dc07604
siddhartha-crypto <siddhartha-crypto@protonmail.com>
2019-09-28 14:44:50 -0800

    begin kelvin.cpp program

---
+++ hw3/kelvin/kelvin.cpp
@@ -0,0 +1,29 @@
+/**
+ * kelvin.cpp
+ * CS 201
+ * Bryan Beus
+ * September 28, 2019
+ * A program to correct the kelvin functions in the hw
+ */
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+using std::noskipws;
+using std::getline;
+
