f84f0697716bb69ac17fa841d6287be07fd9ce67
Siddhartha <siddhartha-crypto@protonmail.com>
2019-10-14 14:13:25 -0800

    split source code into separate header and cpp files

--- hw5/tokenizer/tokenizer.hpp
+++ hw5/tokenizer/tokenizer.hpp
@@ -6,20 +6,41 @@
  * Header file
  */
 
-#ifndef _USERINPUTHPP_
-#define _USERINPUTHPP_
+#ifndef _TOKENIZER_HPP_
+#define _TOKENIZER_HPP_
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::vector;
-using std::string;
-using std::noskipws;
-using std::getline;
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    using std::vector;
+    using std::string;
+    using std::noskipws;
+    using std::getline;
+    using std::istringstream;
+    using std::find;
+
+    bool ReadLine(string &input);
+
+    unsigned StringToTokensWS(const string &input, vector<string> &tokens, bool &isFinished);
+
+    bool containsEnd(string &testSubject);
+
+    void AnalyzeTokens(const vector<string> &tokens);
+
+    int checkType(string &token, string &potType, bool &res, const size_t &longest_length, vector<char> &v);
+
+    void checkWhitespace(string &token, string &potType, bool &res,  const size_t &longest_length);
+
+    void checkStringLiteral(string &token, string &potType, bool &res,  const size_t &longest_length);
+
+    void escapeQuotes(string &token, size_t &tokenLength);
+
+    void printType(string &token,  const size_t &longest_length, string &potType);
 
 #endif
