692ca5a9f8bc95555f20edcba58f7ce8cc2d00ad
Siddhartha <siddhartha-crypto@protonmail.com>
2019-10-14 14:47:03 -0800

    begin dev on database proj

---
+++ hw5/database/database.hpp
@@ -0,0 +1,28 @@
+/**
+ * database.hpp
+ * CS 201
+ * Bryan Beus
+ * October 14, 2019
+ * Header file
+ */
+
+#ifndef _DATABASE_HPP_
+#define _DATABASE_HPP_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+using std::noskipws;
+using std::getline;
+using std::istringstream;
+using std::find;
+
+#endif
