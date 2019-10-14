206574cce5d7a43dc43d3a11f2bcc8dc7cf446dd
Siddhartha <siddhartha-crypto@protonmail.com>
2019-10-14 14:19:51 -0800

    reduce column widths to 20 in tokenizer.cpp

--- hw5/tokenizer/tokenizer.cpp
+++ hw5/tokenizer/tokenizer.cpp
@@ -181,6 +181,6 @@ void escapeQuotes(string &token, size_t &tokenLength) {
 }
 
 void printType(string &token,  const size_t &longest_length, string &potType) {
-    int necessary_width = 35 - potType.length(); 
+    int necessary_width = 20 - potType.length(); 
     cout << "[" << potType << "]" << string(necessary_width, ' ') << '"' << token << '"' << endl; 
 }
