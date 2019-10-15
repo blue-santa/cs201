7476f0e7365485b34a03fb928c26a7099f40830f
Siddhartha <siddhartha-crypto@protonmail.com>
2019-10-15 15:44:48 -0800

    adjust logic in calculations to return proper number of bulls and cows

--- hw4/bulls-and-cows/bulls-and-cowsMain.cpp
+++ hw4/bulls-and-cows/bulls-and-cowsMain.cpp
@@ -1,9 +1,9 @@
 /**
- * userInputMain.cpp
+ * bulls-and-cows.cpp
  * CS 201
  * Bryan Beus
- * September 30, 2019
- * The main file for userInput project
+ * October 15, 2019
+ * The main file for bulls-and-cows
  */
 
 
@@ -32,42 +32,58 @@ using std::stringstream;
 using std::ifstream;
 using std::rand;
 
-void waitForContinue();
-
-void calculateRes(string &pattern, string &userInput, int &bulls, int &cows);
+void waitForContinue(); 
+void clearConsole();
+void calculateRes(string &pattern, string &userInput, int &bulls, int &cows); 
+void setPattern(string &pattern);
+bool testUserInput(string &userInput);
 
 int main(int argc, char **argv) {
 
-    vector<int> ran_nums;
-    srand (time(NULL));
-    
-    for (int i {0}; i < 4; i++) {
-        int temp = rand() % 10;
-        ran_nums.push_back(temp);
-    }
+    // Clear the console
+    clearConsole();
 
+    // Initiate a string to hold the correct pattern
     string pattern = "";
     
-    for (size_t i {0}; i < ran_nums.size(); i++) {
-        stringstream ss;
-        ss << ran_nums.at(i); 
-        pattern = pattern + ss.str();
-    }
-    cout << "Test: " << pattern << endl;
+    // Call the setPattern function to set the pattern
+    setPattern(pattern);
 
-    cout << "Enter four characters to guess the correct pattern. (Hint: All correct answers are digits.)" << endl;
-
-    int bulls;
-    int cows;
-
+    // Initiate endless while loop to repeat until user makes a correct guess
     while (true) {
-        string userInput;
-        getline(cin, userInput);
 
+        // Print the main user prompt
+        cout << endl;
+        cout << endl;
+        cout << "Enter four integers to guess the correct pattern" << endl;
+        cout << endl;
+
+        // Initiate the bulls and cows variables for this round
+        int bulls = 0;
+        int cows = 0; 
+
+        // Collect user input
+        string userInput;
+        getline(cin, userInput); 
+
+        // Ensure that user input is valid
+        bool isValid = testUserInput(userInput);
+
+        // If the input is not valid, restart loop
+        if (!cin || cin.fail() || !isValid) {
+            cin.clear(); 
+            cin.ignore(1000, '\n');
+            cout << "Please enter four integers: ";
+            continue;
+        }
+
+        // Calculate the number of bulls and cows in the user input
         calculateRes(pattern, userInput, bulls, cows);
 
+        // Report the number of bulls and cows
         cout << bulls << " bulls and " << cows << "cows" << endl;
 
+        // If the user guessed correctly, end the program
         if (bulls == 4) {
             cout << "Congrats, you win!" << endl;
             waitForContinue();
@@ -78,22 +94,89 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+// Test whether the user's input is a valid response
+bool testUserInput(string &userInput) {
+
+    // Assume user input is valid
+    bool res = true;
+
+    // Use a istringstream to convert user input to an integer within the appropriate range
+    int testVal; 
+    istringstream iss (userInput); 
+    iss >> testVal;
+
+    // If the string is not a valid reponse, return false
+    if (iss.fail())
+        res = false;
+
+    // If the user entered an integer that cannot be converted to a proper integer, end the program
+    if (testVal > 10000 || testVal < 0)
+        res = false;
+
+    // Return the result
+    return res;
+}
+
+// Clear the console 
+void clearConsole() {
+
+    // Clear the console 
+    cout << "\033[2J\033[1;1H"; 
+}
+
+// Wait 
 void waitForContinue() {
 
     cout << endl << "Press enter to continue...";
     getchar();
 }
 
-void calculateRes(string &pattern, string &userInput, int &bulls, int &cows) {
-
-    for (size_t i {0}; i < 4; i++) {
-        unsigned int temp;
-        temp = pattern.find(userInput[i]);
-
-        if (userInput[i] == pattern[i]) {
-            bulls++;
-        } else if (temp != string::npos) {
+void calculateRes(string &pattern, string &userInput, int &bulls, int &cows) { 
+    string cowCounted = "OOOO";
+     
+    string copyPattern = pattern;
+    bool found = true;
+    int i {0};
+    while(found && i < 4) {
+        unsigned int exists = copyPattern.find(userInput[i]);
+        if ((userInput[i] != pattern[i]) && exists >= 0 && exists < 4) {
+            cout << "Cow: " << userInput[i] << copyPattern[exists] << endl;
             cows++;
+            unsigned int locBull = pattern.find(userInput[i]);
+            cowCounted[locBull] = 'X';
+            copyPattern.erase(exists, 1);
         }
+        i++;
     }
+    
+    for (size_t i {0}; i < 4; i++) {
+        if (userInput[i] == pattern[i]) {
+            cout << "Bull: " << userInput[i] << " " << pattern[i] << endl;
+            bulls++;
+            cout << "cowCounted: " << cowCounted << endl;
+            if (cowCounted[i] == 'X') {
+                cout << "Caught a cow" << endl;
+                cows--;
+            }
+        } 
+    } 
+
+}
+
+void setPattern(string &pattern) {
+    vector<int> ran_nums;
+    srand (time(NULL));
+    
+    for (int i {0}; i < 4; i++) {
+        int temp = rand() % 10;
+        ran_nums.push_back(temp);
+    }
+
+    
+    for (size_t i {0}; i < ran_nums.size(); i++) {
+        stringstream ss;
+        ss << ran_nums.at(i); 
+        pattern = pattern + ss.str();
+    } 
+    cout << "Pattern: " << pattern;
 }
