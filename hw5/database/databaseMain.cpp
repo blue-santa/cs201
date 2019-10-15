1f371e6b5bffe2fa98744adceb9aa8b66704ce30
Siddhartha <siddhartha-crypto@protonmail.com>
2019-10-14 16:27:25 -0800

    add tao te ching files to database and initiate database in databasMain.cpp

--- hw5/database/databaseMain.cpp
+++ hw5/database/databaseMain.cpp
@@ -1,35 +1,93 @@
 /**
- * userInputMain.cpp
+ * databaseMain.cpp
  * CS 201
  * Bryan Beus
- * September 30, 2019
- * The main file for userInput project
+ * October 14, 2019
+ * The main file for database project
  */
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <map>
+#include <fstream>
 
-#include "userInput.hpp"
+// #include "database.hpp"
 
 using std::cout;
+using std::cerr;
 using std::cin;
 using std::endl;
 using std::vector;
 using std::string;
 using std::noskipws;
 using std::getline;
+using std::find;
+using std::istringstream;
+using std::ifstream;
+
+bool createRecord() {
+
+    return true;
+}
+
+bool readRecord() {
+
+    return true;
+}
+
+bool updateRecord() {
+
+    return true;
+}
+
+bool deleteRecord() {
+
+    return true;
+} 
 
 int main(int argc, char **argv) {
 
-	vector<string> userInput;
-	vector<string> userInput2;
-	vector<string> userInput3;
+    vector<vector <string> > taoTeChing (81);
 
-	exercise1(userInput);
-	exercise2(userInput, userInput2);
-	exercise3(userInput, userInput3);
+    string filename = "tao_te_ching.txt";
+
+    // Load Tao Te Ching into database
+    ifstream in(filename);
+
+    if (!in) {
+        cerr << "Cannot open file: " << filename << endl;
+        return 0;
+    }
+
+    string line;
+    int chapterCount {-1};
+
+    while (getline(in, line)) {
+       istringstream instream(line);
+       string firstWord;
+
+       instream >> firstWord;
+       if (firstWord == "Chapter") {
+            chapterCount++;
+       } else {
+            taoTeChing[chapterCount].push_back(line);
+            cout << line << endl;
+       }
+    }
+
+
+    for (size_t i {0}; i < taoTeChing.size(); i++) {
+        cout << "Chapter " << i << endl;
+        cout << endl;
+        for (size_t j {0}; j < taoTeChing.at(i).size(); j++) {
+            cout << taoTeChing[i].at(j) << endl;
+        }
+        cout << endl;
+        cout << endl;
+    }
 
 	return 0;
 }
