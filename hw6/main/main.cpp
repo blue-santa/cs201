d9b3bb2106e785e7f3b11d916a6af6b30be004d7
siddhartha <siddhartha-crypto@protonmail.com>
2019-11-17 13:19:53 -0900

    create process for uniform distribution and printout in main of hw6

--- hw6/main/main.cpp
+++ hw6/main/main.cpp
@@ -9,49 +9,61 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int RandomBetweenU(int first, int last, std::default_random_engine &e1);
+int RandomBetweenU(int first, int last, std::mt19937 &e1);
 int RandomBetweenN(int first, int last);
 int RandomBetween(int first, int last);
 void PrintDistribution(const std::map<int, int> &numbers);
  
 int main()
 {
-    // Seed with a real random value, if available
-    std::random_device r;
+	// Declare endpoints of the range for the distributions
 
-    int first = 1;
-    int last = 6;
- 
-    // Choose a random mean between 1 and 6
-    std::default_random_engine e1(r());
-    std::uniform_int_distribution<int> uniform_dist(first, last);
+	int first = 1;
+	int last = 6;
 
-    int test_one = RandomBetweenU(first, last, e1);
-    
-    cout << "Test_one: " << test_one << endl;
+	// Declare pseudo-random device for creating seeds
+	std::random_device r;
+
+	// Create a seed sequence to feed to the generator
+	std::seed_seq seedObj{r(), r(), r(), r(), r(), r(), r(), r()}; 
+
+	// Declare random-number generator and provide with seedObj sequence
+	std::mt19937 e1(seedObj);
+
+	// Create uniform historgram
+	std::map<int, int> uniformHistogram;
+	for (int i = 0; i < 10000; i++) {
+		++uniformHistogram[std::round(RandomBetweenU(first, last, e1))];
+	}
+
+	for (auto p: uniformHistogram) {
+		cout << std::fixed << std::setprecision(1) << std::setw(2) << p.first << ' ' << std::string(p.second/200, '*') << endl;
+	}
+
+	int test_one = RandomBetweenU(first, last, e1); 
+	cout << "Test_one: " << test_one << endl;
 
 
-    // Previous example code
-    int mean = uniform_dist(e1);
-    std::cout << "Randomly-chosen mean: " << mean << '\n';
- 
-    // Generate a normal distribution around that mean
-    std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()}; 
-    std::mt19937 e2(seed2);
-    std::normal_distribution<> normal_dist(mean, 2);
- 
-    std::map<int, int> hist;
-    for (int n = 0; n < 10000; ++n) {
-        ++hist[std::round(normal_dist(e2))];
-    }
-    std::cout << "Normal distribution around " << mean << ":\n";
-    for (auto p : hist) {
-        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
-    }
+	// Previous example code
+	std::uniform_int_distribution<int> uniform_dist(first, last);
+	int mean = uniform_dist(e1);
+	std::cout << "Randomly-chosen mean: " << mean << '\n';
+
+	// Generate a normal distribution around that mean
+	std::normal_distribution<> normal_dist(mean, 2);
+
+	std::map<int, int> hist;
+	for (int n = 0; n < 10000; ++n) {
+		++hist[std::round(normal_dist(e1))];
+	}
+	std::cout << "Normal distribution around " << mean << ":\n";
+	for (auto p : hist) {
+		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+			  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+	}
 }
 
-int RandomBetweenU(int first, int last, std::default_random_engine &e1) {
+int RandomBetweenU(int first, int last, std::mt19937 &e1) {
 
 	std::uniform_int_distribution<int> uniform_dist(first, last);
 	int val = uniform_dist(e1);
