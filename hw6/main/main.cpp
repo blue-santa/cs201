d9e4326aa3771af0a4e3193f11481d149531b060
siddhartha <siddhartha-crypto@protonmail.com>
2019-11-17 13:58:26 -0900

    normal and std::rand() distributions are now functional in main.cpp of hw6

--- hw6/main/main.cpp
+++ hw6/main/main.cpp
@@ -4,13 +4,14 @@
 #include <map>
 #include <random>
 #include <cmath>
+#include <stdlib.h>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 int RandomBetweenU(int first, int last, std::mt19937 &e1);
-int RandomBetweenN(int first, int last);
+int RandomBetweenN(int first, int last, std::mt19937 &e1);
 int RandomBetween(int first, int last);
 void PrintDistribution(const std::map<int, int> &numbers);
  
@@ -36,31 +37,28 @@ int main()
 		++uniformHistogram[std::round(RandomBetweenU(first, last, e1))];
 	}
 
-	for (auto p: uniformHistogram) {
-		cout << std::fixed << std::setprecision(1) << std::setw(2) << p.first << ' ' << std::string(p.second/200, '*') << endl;
+	// Print the uniform histogram
+	PrintDistribution(uniformHistogram);
+
+	// Create normal histogram
+	std::map<int, int> normalHistogram;
+	for (int i = 0; i < 10000; i++) {
+		++normalHistogram[std::round(RandomBetweenN(first, last, e1))];
 	}
 
-	int test_one = RandomBetweenU(first, last, e1); 
-	cout << "Test_one: " << test_one << endl;
+	// Print the normal histogram
+	PrintDistribution(normalHistogram);
 
-
-	// Previous example code
-	std::uniform_int_distribution<int> uniform_dist(first, last);
-	int mean = uniform_dist(e1);
-	std::cout << "Randomly-chosen mean: " << mean << '\n';
-
-	// Generate a normal distribution around that mean
-	std::normal_distribution<> normal_dist(mean, 2);
-
-	std::map<int, int> hist;
-	for (int n = 0; n < 10000; ++n) {
-		++hist[std::round(normal_dist(e1))];
+	// Create standard rand() histogram
+	std::map<int, int> standardHistogram;
+	for (int i = 0; i < 10000; i++) {
+		++standardHistogram[std::round(RandomBetween(first, last))];
 	}
-	std::cout << "Normal distribution around " << mean << ":\n";
-	for (auto p : hist) {
-		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-			  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
-	}
+
+	// Print the normal histogram
+	PrintDistribution(standardHistogram);
+
+	return 0;
 }
 
 int RandomBetweenU(int first, int last, std::mt19937 &e1) {
@@ -72,20 +70,28 @@ int RandomBetweenU(int first, int last, std::mt19937 &e1) {
 
 }
 
-int RandomBetweenN(int first, int last) {
+int RandomBetweenN(int first, int last, std::mt19937 &e1) {
 
-	int val = 0;
+	std::normal_distribution<> normal_dist(3.5, 1);
+	int val = normal_dist(e1);
+
 	return val;
 	
 }
 
 int RandomBetween(int first, int last) {
-	int val = 0;
+	int val = (std::round(std::rand()));
+	val = val % 10;
 	return val;
 
 } 
 
 void PrintDistribution(const std::map<int, int> &numbers) {
 
+	for (auto p: numbers) {
+		cout << std::fixed << std::setprecision(1) << std::setw(2) << p.first << ' ' << std::string(p.second/20, '*') << endl;
+	}
+
+	cout << endl;
 }
  
