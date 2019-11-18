16436984bc4363a187ffd4e3b9bb78dd632b0b85
siddhartha <siddhartha-crypto@protonmail.com>
2019-11-17 21:54:16 -0900

    add the four functions to the test template

---
+++ hw6/catch/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <math.h>
+#include <stdio.h>
+#include <numeric>
+
+#define _USING_MATH_DEFINES
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::sin;
+using std::atan2;
+using std::accumulate;
+using std::inner_product;
+using std::vector;
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Common math functions perform", "[math]" ) {
+	double sinVal = sin(M_PI);
+
+	double x, y, atan2Val;
+	atan2Val = atan2 (x, y) * 180 / M_PI;
+
+	int init = 100;
+	vector<int> accPreVals{10, 20, 30, 40};
+	int accVal = accumulate(accPreVals.begin(), accPreVals.end(), init);
+		
+	
+	vector<int> a {0, 1, 2, 3, 4};
+	vector<int> b {5, 6, 7, 8, 9};
+	int innProVal = inner_product(a.begin(), a.end(), b.begin(), 0);
+
+    REQUIRE( sin(M_PI) == sinVal );
+    REQUIRE( atan2 (x, y) * 180 / M_PI == atan2Val );
+    REQUIRE( accumulate(accPreVals.begin(), accPreVals.end(), init) == accVal );
+    REQUIRE( inner_product(a.begin(), a.end(), b.begin(), 0) == innProVal );
+}
+ 
