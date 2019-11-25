00048c3d573612baaa9ad347cc0189ad28dbaf7c
siddhartha <siddhartha-crypto@protonmail.com>
2019-11-24 20:14:05 -0900

    add mandelbrot project to hw7

--- hw7/mandelbrot/main.cpp
+++ hw7/mandelbrot/main.cpp
@@ -5,12 +5,13 @@
  * The main file for the ascii art project
  */
 
+#include <cmath>
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <sstream>
 
-#include "Color3.hpp" 
+#include "Color3.hpp"
 #include "Image3.hpp"
 
 using std::cout;
@@ -21,52 +22,99 @@ using std::ifstream;
 using std::ofstream;
 
 using std::vector;
-using std::string;
+using std::string; 
+
+// Process the mandelbrot image
+void process_mandelbrot(Image3& image);
 
 int main() {
 
-	// Declare file paths
-	string input_path = "parrot.ppm";
-	string output_path = "test.ppm";
-	string ascii_output_path = "ascii.txt";
+	// Set image dimesions
+	double scale = 0.2;
+	unsigned width = (unsigned)(std::round(5120 * scale));
+	unsigned height = (unsigned)(std::round(2098 * scale));
 
-	// Create empty image object for manipulation
-	Image3 image(0, 0); 
+	// Create default image
+	Image3 image(width, height);
 
-	// Create input stream from input path
-	ifstream fin2(input_path);
+	// Process the image with mandelbrot calculations
+	process_mandelbrot(image);
 
-	if (!fin2)
-		cout << "no fin" << endl;
+	// Create output stream and save file
+	string output_file("mandelbrot.ppm");
+	ofstream fout(output_file);
 
-	// Import the file into the image object
-	fin2 >> image;
-
-	// Ensure fin2 is closed
-	fin2.close();
-
-	// Create output path for the test PPM
-	// This allows us to ensure that the file is loaded and writing correctly
-	ofstream fout2(output_path);
-
-	if (!fout2)
+	if (!fout) {
 		cout << "no fout" << endl;
+		return 0;
+	}
 
-	// Write the image object to the file
-	fout2 << image;
+	fout << image;
 
-	// Ensure output is closed
-	fout2.close();
-
-	// Create output stream for ascii art
-	ofstream fout3(ascii_output_path);
-
-	if (!fout3)
-		cout << "no fout3" << endl;
-
-
-	// Print the ascii art to the file
-	image.printASCII(fout3);
-
-	return 0; 
+	return 0;
 }
+
+// Process the mandelbrot image
+void process_mandelbrot(Image3& image) {
+
+	// Allow for w, h variables in int form
+	int w = (int)image.w;
+	int h = (int)image.h;
+
+	// For each pixel(Px, Py) on the screen, do:
+	for (int i = 0; i < w; i++) {
+		for (int j = 0; j < h; j++) {
+
+			// Set x = i iteration
+			// Set y = j iteration
+			int x = i;
+			int y = j;
+
+			// x0 = scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.5, 1))
+			double x0 = ( (double)x / (double)w) * 3.5 - 2.5;
+
+			// y0 = scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1, 1))
+			double y0 = ( (double)y / (double)h) * 2 - 1;
+
+			// x = 0.0
+			double x_val = 0.0;
+
+			// y = 0.0
+			double y_val = 0.0;
+
+			// iteration = 0
+			int iteration = 0;
+
+			// max_iteration = 1000
+			int max_iteration = 1000;
+
+			// while (x*x + y*y <= 2*2 AND iteration < max+iteration
+			while (x_val*x_val + y_val * y_val <= 2*2*2*2 && iteration < max_iteration) {
+
+				// xtemp = x*x - y*y + x0
+				double x_temp = (double) x_val*x_val - y_val*y_val + x0;
+
+				// y = 2 * x * y + y0
+				double y_temp = (double) 2 * x_val * y_val + y0;
+				y_val = y_temp;
+
+				// x = xtemp
+				x_val = x_temp;
+
+				// iteration = iteration + 1
+				iteration++;
+			}
+
+
+			// color = palette[iteration]
+			int r, g, b;
+			r = iteration * 15;
+			g = r / 3;
+			b = g / 2;
+
+			// plot(Px, Py, color)
+			image.setPixel(x, y, Color3(r, g, b));
+		}
+	}
+}
+
