4584829f6835659e88bc78d6104d93b08b766294
siddhartha <siddhartha-crypto@protonmail.com>
2019-12-06 15:47:17 -0900

    begin hw8

---
+++ hw8/thermostat/main.cpp
@@ -0,0 +1,42 @@
+/*
+ * main.cpp
+ * CS 201
+ * December 6, 2019
+ * Bryan Beus
+ * Main file for themostat main project in hw8
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <vector>
+#include <string>
+#include <map>
+
+using std::vector;
+using std::pair;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::make_pair;
+using std::for_each;
+using std::getline;
+using std::istringstream;
+using std::ostringstream;
+using std::to_string;
+using std::find;
+using std::map;
+using std::setw;
+using std::left;
+using std::getline;
+
+int main() {
+
+	return 0; 
+}
+
+
